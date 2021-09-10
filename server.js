4173f664bef66f3726cdb6e41a7b267ad6a0c575
sattep91 <67924622+sattep91@users.noreply.github.com>
2021-09-09 21:36:28 -0500

    Added database connection

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import connectDatabase from './config/db';
+
+//initialize express applications
+const app = express();
+
+//Connect database
+connectDatabase();
+
+//API endpoints
+app.get('/', (req, res) =>
+  res.send('http get request sent to root api endpoint')
+);
+
+//connect listener
+app.listen(3000, () => console.log('Express server running on port 3000'));
