84947e0ce29f92a5f1d0dd5c94b7540fae634f8b
sattep91 <67924622+sattep91@users.noreply.github.com>
2021-09-20 05:58:09 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connectDatabase from './config/db';
-
+import {check, validationResults} from 'express-validator';
 //initialize express applications
 const app = express();
 
@@ -25,9 +25,16 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', (req, res) => {
+app.post(
+  '/api/users', 
+  [
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email address').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+  ],
+  (req, res) => {
   console.log(req.body);
-  res.send(req, body);
+  res.send(req.body);
 });
 
 //connect listener
