3834a571ebc29452859d110d2affd44d5d75a2b1
sattep91 <67924622+sattep91@users.noreply.github.com>
2021-09-21 22:35:50 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDatabase from './config/db';
 import {check, validationResults} from 'express-validator';
+
 //initialize express applications
 const app = express();
 
@@ -15,8 +16,6 @@ app.use(express.json({extended: false}));
  * @route GET /
  * @desc Test endpoint
  */
-
-
 app.get('/', (req, res) =>
   res.send('http get request sent to root api endpoint')
 );
@@ -28,14 +27,24 @@ app.get('/', (req, res) =>
 app.post(
   '/api/users', 
   [
-    check('name', 'Please enter your name').not().isEmpty(),
+    check('name', 'Please enter your name')
+    .not()
+    .isEmpty(),
     check('email', 'Please enter a valid email address').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+    check(
+      'password', 
+      'Please enter a password with 6 or more characters'
+      ).isLength({min: 6})
   ],
   (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-});
+    const errors = validationResults(req);
+    if(!errors.isEmpty()) {
+      return res.status(422).json({errors: errors.array() });
+    } else{
+      return res.send(req.body);
+    }
+  }
+);
 
 //connect listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
